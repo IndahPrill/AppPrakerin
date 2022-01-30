20b5df889dbeb0f85f687db66af3381a7171a575
didinirfandy <didinirfnady16@gmail.com>
2022-01-30 14:00:48 +0700

    update model koor

--- application/models/Koordinator_model.php
+++ application/models/Koordinator_model.php
@@ -7,4 +7,25 @@ class Koordinator_model extends CI_Model
     {
         return $this->db->query("SELECT * FROM m_lokasi")->result_array();
     }
+
+    public function addLksPkl($data)
+    {
+        return $this->db->insert('m_dosen', $data);
+    }
+
+    public function editLksPkl($id_dsn, $data)
+    {
+        $this->db->where('id_dsn', $id_dsn);
+        $hsl = $this->db->update('m_dosen', $data);
+
+        return $hsl;
+    }
+
+    public function deleteLksPkl($id_dsn)
+    {
+        $this->db->where('id_dsn', $id_dsn);
+        $hsl = $this->db->delete('m_dosen');
+
+        return $hsl;
+    }
 }
