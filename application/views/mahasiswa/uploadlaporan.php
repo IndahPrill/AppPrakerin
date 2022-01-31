7e794e1733d4bf3c7779a89f1a996388162380a8
didinirfandy <didinirfnady16@gmail.com>
2022-01-31 17:50:58 +0700

    upload laporan di hapus komentarnya

--- application/views/mahasiswa/uploadlaporan.php
+++ application/views/mahasiswa/uploadlaporan.php
@@ -7,7 +7,7 @@
 
         <div class="card shadow mb-4">
             <div class="card-body">
-                <form action="<?= base_url('mahasiswa/uploadLaporan');?>" method="post">
+                <form action="<?= base_url('mahasiswa/uploadLaporan'); ?>" method="post">
                     <div class="form-group">
                         <label for="namaMHS">Nama Mahasiswa</label>
                         <input type="text" class="form-control" id="namaMHS" name="nama_mhs">
@@ -30,41 +30,7 @@
                 </form>
             </div>
         </div>
-
-        <!-- <div class="row">
-            <div class="col-lg-6">
-
-                <?= $this->session->flashdata('message');  ?>
-
-                <form action="<?= base_url('mahasiswa/changepassword');?>" method="post">
-                    <div class="form-group">
-                        <label for="current_password">Nama Mahasiswa</label>
-                        <input type="password" class="form-control" id="current_password" name="current_password">
-                        <?=form_error('current_password', '<small class="text-danger pl-3">','</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                        <label for="new_password1">NPM</label>
-                        <input type="password" class="form-control" id="new_password1" name="new_password1">
-                        <?=form_error('new_password1', '<small class="text-danger pl-3">','</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                        <label for="new_password2">Judul Laporan</label>
-                        <input type="password" class="form-control" id="new_password2" name="new_password2">
-                        <?=form_error('new_password2', '<small class="text-danger pl-3">','</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                    <label for="formFile" class="form-label">Upload Laporan PKL</label>
-                    <input class="form-control" type="file" id="formFile">
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Upload</button>
-                    </div>
-                </form>
-            </div>
-        </div> -->
     </div>
     <!-- /.container-fluid -->
-</div>
-<!-- End of Main Content -->
-
-            
+    </div>
+    <!-- End of Main Content -->
